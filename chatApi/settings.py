57bdbaff652a110a8536364b9633d725fec4695b
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-17 11:36:47 +0100

    project has been deployed to aws

--- chatApi/settings.py
+++ chatApi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +122,34 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+S3_BUCKET_URL= config('S3_BUCKET_URL')
+STATIC_ROOT= 'staticfiles'
+
+AWS_ACCESS_KEY_ID= config('AWS_S3_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= config('AWS_S3_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= config('AWS_STORAGE_BUCKET_NAME')
+AWS_HOST_REGION= config('AWS_HOST_REGION')
+AWS_S3_CUSTOM_DOMAIN= '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL= None
+
+AWS_LOCATION= 'static'
+MEDIA_URL= '/media/'
+
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'chatapi/static'),
+]
+
+STATIC_URL= 'https://%s%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+STATICFILES_STORAGE= 'storage.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_OBJECT_PARAMETERS= {
+    
+    'CacheControl': 'max-age= 86400',
+}
+
+DEFAULT_FILE_STORAGE= 'chatapi.storage_backends.MediaStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
